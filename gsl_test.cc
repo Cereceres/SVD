091b16cd3dd099c9c645139054d45874ea56e137
Jesus Cereceres <jesus.edelcereceres@gmail.com>
2015-10-07 15:07:28 -0500

    the array object is now passed from js to c++

--- gsl_test.cc
+++ gsl_test.cc
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <gsl/gsl_sf_bessel.h>
 #include <nan.h>
+using namespace v8;
 
-void prueba (const Nan::FunctionCallbackInfo<v8::Value>& info) 
-{
-  double x = 5.0;
+void prueba (const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  double x ; int i; Handle<Array> array;
+  if (info[0]->IsArray()) {
+     array = Handle<Array>::Cast(info[0]);
+    int l = (int) array->Length();
+    for (i = 0; i < l; i++) {
+     x = array->Get(i)->NumberValue();
+     printf("x=%f\n",x );
+    }
+  }
+   x = array->Get(0)->NumberValue();
   double y = gsl_sf_bessel_J0 (x);
   printf ("J0(%g) = %.18e\n", x, y);
 }
@@ -12,9 +21,6 @@ void prueba (const Nan::FunctionCallbackInfo<v8::Value>& info)
 void CreateFunction(const Nan::FunctionCallbackInfo<v8::Value>& info) {
   v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(prueba);
   v8::Local<v8::Function> fn = tpl->GetFunction();
-
-  // omit this to make it anonymous
-  fn->SetName(Nan::New("theFunction").ToLocalChecked());
 
   info.GetReturnValue().Set(fn);
 }
--- gsl_test.js
+++ gsl_test.js
@@ -1,4 +1,3 @@
 var addon = require('bindings')('addon');
-
 var fn = addon();
-fn(); // 'hello world'
+fn([7,5,6]); // 'hello world'
