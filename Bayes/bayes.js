30673102b3d6b0fce50a6074df35a82ca858b44f
Jesus Cereceres <jesus.edelcereceres@gmail.com>
2015-10-17 19:28:23 -0500

    the rieman problem is not solved

---
+++ Bayes/bayes.js
@@ -0,0 +1,40 @@
+'use strict';
+
+//Bayes generate the aleatory data
+var create = require('../Riemann/riemann')('data', 'stats');
+var A = [], sigma, media, sqrt3 = Math.sqrt(3);
+var  _sqrt3 = 2 * sqrt3, i = 0, j = 0, m = 10, n = 10, save, cb, end, time, start;
+
+var rand = function(mu, sigma) {
+  return mu - sqrt3 * sigma + _sqrt3 * Math.random() * sigma;
+};
+
+save = function() {
+  i++;
+  if (i < n) {
+    A = [];
+    for (j = 0; j < m; j++) {
+      sigma = 10 * Math.random();
+      media = 10 * Math.random();
+      A[j] = rand(media, sigma);
+    }
+
+    create({data:A}, cb);
+  }else {
+    end = new Date().getTime();
+    time = end - start;
+    console.log('Done All with ' + n + ' data generated at Execution time: ' + time / 1000);
+    return;
+  }
+};
+
+cb = function(err) {
+  if (err) {
+    console.log('err=', err);
+  }else {
+    save();
+  }
+};
+
+start = new Date().getTime();
+save();
