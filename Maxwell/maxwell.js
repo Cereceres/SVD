c1b1e58a20cdd20a934cf16563c7d60cc8d64f0f
Jesus Cereceres <jesus.edelcereceres@gmail.com>
2015-10-17 19:29:43 -0500

    the problem with riemann is not solved

--- Maxwell/maxwell.js
+++ Maxwell/maxwell.js
@@ -1,25 +1,30 @@
 'use strict';
+
+//Maxwell make the analisys of data
 var newton = require('bindings')('newton');
 var f_newton = newton();
 var data_model = require('./data_model');
-var A=[],B=[];
-  var start = new Date().getTime();
-  data_model.findRandom({},{'data': 1, '_id': 0},{ skip: 10, limit: 1000000 },function (error,res) {
+var A = [], B = [];
+var start = new Date().getTime();
+data_model.findRandom({}, {data: 1, _id: 0}, { skip: 10, limit: 1000000 }, function(error, res) {
     if (error) {
-      console.log('error = ',error);
+      console.log('error = ', error);
       return;
     }
-    console.log('numero de dato=',res);
-    res.forEach(function (item) {
+
+    console.log('numero de dato=', res);
+    res.forEach(function(item) {
       A.push(item.data);
     });
+
     var l = A[0].length;
     for (var i = 0; i < l; i++) {
-      B[i]=20 *Math.random();
+      B[i] = 20 * Math.random();
     }
+
     var end = new Date().getTime();
     var time = end - start;
-    console.log('p_x=',f_newton(A,0.8,B).p_x);
+    console.log('p_x=', f_newton(A, 0.8, B).p_x);
     console.log('Execution time: ' + time / 1000);
 
   });
--- Riemann/riemann.js
+++ Riemann/riemann.js
@@ -1,36 +1,44 @@
 'use strict';
-var   mongoose = require('./mongoose');
- var Statsaving= function (namedata, namestats) {
-   var doc;
-  var Schema = mongoose.Schema;
-  this.schemadata =  new Schema({
-    data : Array
+var   mongoose = require('../mongoose');
+var Schema = mongoose.Schema;
+var schemadata =  new Schema({
+  data: Array,
+});
+
+var schemastats =  new Schema({
+    sigma: Array,
+    media: Array,
+    N: Array,
   });
-  this.Modeldata = mongoose.model(namedata,this.schemadata);
-  this.schemastats =  new Schema({
-    sigma : Array ,
-    media : Array,
-    N : Array,
-  });
-  this.schemastats.post('find', function(error,stats) {
-      var l = this.data.length;
-      for (var i = 0; i < l; i++) {
-          stats.media[i] =(stats.media[i]*stats.N[i]+this.doc.data[i])/(stats.N[i]+1);
-          stats.sigma[i] =Math.sqrt((stats.sigma[i]*stats.sigma[i]*(stats.N[i]-1)+(this.doc.data[i]-stats.media[i])*(this.doc.data[i]-stats.media[i]))/(stats.N[i]));
-          (stats.N[i])++;
+
+// Riemann module make the stats into de data
+var Statsaving = function(namedata, namestats) {
+  var Modeldata = mongoose.model(namedata, schemadata);
+  var Modelstats = mongoose.model(namestats, schemastats);
+  return function(tosave, cb) {
+    var doc = new Modeldata(tosave);
+    Modelstats.find({}, function(error, stats) {
+      if (error) {
+        console.log('error into save doc = ', error); return;
       }
-      this.doc.save.call(this.cb,function (err) {
-        stats.save(this);
+
+      var l = stats.length;
+      for (var i = 0; i < l; i++) {
+        stats.media[i] = (stats.media[i] * stats.N[i] + doc.data[i]) / (stats.N[i] + 1);
+        stats.sigma[i] = Math.sqrt((stats.sigma[i] * stats.sigma[i] * (stats.N[i] - 1) + (doc.data[i] - stats.media[i]) * (doc.data[i] - stats.media[i])) / (stats.N[i]));
+        (stats.N[i])++;
+      }
+
+      doc.save(function(err) {
+        if (err) {
+          console.log('error into save doc = ', err); return;
+        }
+
+        stats.save(cb);
       });
-
-  });
-  this.Modelstats = mongoose.model(namestats,this.schemastats);
-
-  this.create = function (tosave,cb) {
-    doc = new this.Modeldata(tosave);
-    this.Modelstats.find.call({ doc:doc, cb :cb},{});
+    });
   };
 
 };
 
-module.exports.Statsaving = Statsaving;
+module.exports = Statsaving;
