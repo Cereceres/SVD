5c9c0bed5fd05daac962e7ba23bacfd0d5f6f533
Jesus Cereceres <jesus.edelcereceres@gmail.com>
2015-10-18 22:18:53 -0500

    Curie is init to save the matrix stats

--- Maxwell/maxwell.js
+++ Maxwell/maxwell.js
@@ -1,33 +1,22 @@
 'use strict';
 
 //Maxwell make the analisys of data
-var newton = require('bindings')('newton');
-var f_newton = newton();
 var Riemann = new require('../Riemann/riemann');
 var riemann = new Riemann('data', 'stats');
 var data_model = riemann.Modeldata;
-var Noether = require('../Noether/noether');
-var random = Noether.random;
-var A = [], B = [];
-var start = new Date().getTime();
-data_model.findRandom({}, {data: 1, _id: 0}, { skip: 10, limit: 1000 }, function(error, res) {
-    if (error) {
-      console.log('error = ', error);
-      return;
-    }
+var A = [];
+module.exports.sample = function(Nsample, cb) {
+  data_model.findRandom({}, {data: 1, _id: 0}, { skip: 10, limit: Nsample },
+    function(error, res) {
+      if (error) {
+        console.log('error = ', error);
+        return;
+      }
 
-    res.forEach(function(item) {
-      A.push(item.data);
+      res.forEach(function(item) {
+        A.push(item.data);
+      });
+
+      cb(A);
     });
-
-    var l = A[0].length;
-    for (var i = 0; i < l; i++) {
-      B[i] = 20 * random();
-    }
-
-    var end = new Date().getTime();
-    var time = end - start;
-    console.log('p_x=', f_newton(A, 0.8, B).p_x);
-    console.log('Execution time: ' + time / 1000);
-
-  });
+};
