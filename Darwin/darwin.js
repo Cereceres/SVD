b5971991fe033b46e98daee697991cad494a028c
Jesus Cereceres <jesus.edelcereceres@gmail.com>
2015-10-21 18:14:20 -0500

    Darwin is upgrade

--- Darwin/darwin.js
+++ Darwin/darwin.js
@@ -17,12 +17,12 @@ var pca_sample = function(timeupgrade, sizesample, options) {
     samplig(sizesample, function(Sample) {
       statsmodel.findOne({}, function function_name(err, stats) {
         pca = gsl_pca(Sample, limit, [stats.media, stats.sigma]);
-        pcamodel.findOneAndUpdate({}, { V_T_matrix: pca.V_trans, S_vector: pca.S_corr },{new : true,upsert: true}, function(error,doc) {
+        pcamodel.findOneAndUpdate({}, { V_T_matrix: pca.V_trans, S_vector: pca.S_corr },{new : true,upsert: true}, function(error) {
 
           if (error) {
             console.log('Error on save de PCA', error);
           }
-          console.log('matrix is upgrade',doc);
+          console.log('matrix is upgrade');
         });
       });
     });
