af7bc2ab5c3cfd33a0956e1e8f0e4dcaadd27260
cereceres <jesus.edelcereceres@gmail.com>
2016-04-08 18:26:15 -0500

    the add method is available

--- bigN.js
+++ bigN.js
@@ -1,37 +1,104 @@
 'use strict'
 class bigN {
-  constructor( number ) {
+  constructor(number) {
     number = number || 0
-    this.number = number.toString( ).split( '' )
+    this.sign = 1
+    let str = number.toString();
+    this.number = str.split('')
   }
-  add( number ) {
-    let num = number.toString( ).split( '' )
+  add(number) {
+    let num = number.toString().split('')
     let max = this.number.length > num.length ? this.number.length : num.length
-    let res = [ ],
-      a, b
-    for ( var i = 1; i <= max; i++ ) {
-      a = this.number[ ( this.number.length - i ) ] === undefined ? +( this.number[
-        this.number.length - i ] ) : 0
-      b = num[ num.length - i ] === undefined ? +( num[ num.length - i ] ) :
-        0
-      res[ max - i ] = a + b
+    let a, b
+    let _res = 0,
+      res = []
+    for (var i = 1; i <= max; i++) {
+      a = this.number[this.number.length - i] !== undefined ? +(this.number[
+        this.number.length - i]) : 0
+      b = num[num.length - i] !== undefined ? +(num[num.length - i]) : 0
+      res[max - i] = a + b + _res;
+      if (res[max - i] > 9) {
+        _res = 1
+        res[max - i] = res[max - i] % 10
+      } else {
+        _res = 0
+      }
+    }
+    delete this.number
+    this.number = res
+    return this
+  }
+  multiply(number) {
+    let num = number.toString().split('')
+    let max = this.number.length + num.length
+    let a, b
+    let _res = 0,
+      res = []
+    for (var i = 1; i <= this.number.length; i++) {
+      a = +(this.number[this.number.length - i])
+      for (var j = 1; j <= num.length; j++) {
+        b = +(num[num.length - j])
+        res[max - i] = a * b + _res;
+        if (res[max - i] > 9) {
+          _res = Math.floor(res[max - i] / 10)
+          res[max - i] = res[max - i] % 10
+        } else {
+          _res = 0
+        }
+      }
 
     }
-  }
-  multiply( number ) {
-    let num = number.toString( ).split( '' )
+    delete this.number
+    this.number = res
+    return this
 
   }
-  rest( number ) {
-    let num = number.toString( ).split( '' )
+  rest(number) {
+    let num = number.toString().split('')
+    let max = this.number.length > num.length ? this.number.length : num.length
+    let a, b
+    let _res = 0,
+      res = []
+    for (var i = 1; i <= max; i++) {
+      a = this.number[this.number.length - i] !== undefined ? +(this.number[
+        this.number.length - i]) : 0
+      b = num[num.length - i] !== undefined ? +(num[num.length - i]) : 0
+      res[max - i] = a + b + _res;
+      if (res[max - i] > 9) {
+        _res = 1
+        res[max - i] = res[max - i] % 10
+      } else {
+        _res = 0
+      }
+    }
+    delete this.number
+    this.number = res
+    return this
 
   }
-  div( number ) {
-    let num = number.toString( ).split( '' )
+  div(number) {
+    let num = number.toString().split('')
+    let max = this.number.length > num.length ? this.number.length : num.length
+    let a, b
+    let _res = 0,
+      res = []
+    for (var i = 1; i <= max; i++) {
+      a = this.number[this.number.length - i] !== undefined ? +(this.number[
+        this.number.length - i]) : 0
+      b = num[num.length - i] !== undefined ? +(num[num.length - i]) : 0
+      res[max - i] = a + b + _res;
+      if (res[max - i] > 9) {
+        _res = 1
+        res[max - i] = res[max - i] % 10
+      } else {
+        _res = 0
+      }
+    }
+    delete this.number
+    this.number = res
+    return this
 
   }
 }
 
-console.log( new bigN( 3453454 ).number );
-
-console.log( -3 % 6 );
+console.log(new bigN(12).multiply(23).number);
